901967e31708863218b32e68abe44fb788be5e8e
20thmarcus <marcusfadrigalan@gmail.com>
2024-05-20 00:45:23 +0800

    Fifth Ver

--- components/components/PDA.js
+++ components/components/PDA.js
@@ -2,59 +2,53 @@ import {
   Box,
   VStack,
   Button,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverHeader,
-  PopoverBody,
-  PopoverArrow,
-  PopoverCloseButton,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react";
 
 const PDA = ({ prob2 }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <Popover isLazy placement="top">
-        <PopoverTrigger>
-          <Button
-            mx="2"
-            bg="#151515"
-            color="gray.100"
-            _hover={{ bg: "blue.700" }}
-          >
-            Show PDA
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent
-          maxW={["17em", null, "16em", null, null, "80em"]}
-          _focus=""
-          bg="#151515"
-          fontSize={["0.7em", "0.7em", "0.9em"]}
-        >
-          <PopoverArrow />
-          <PopoverCloseButton />
-          <PopoverHeader fontWeight="semibold" color="gray.100">
-            Pushdown Automata
-          </PopoverHeader>
-          <PopoverBody align="center">
+      <Button
+        mx="2"
+        bg="#151515"
+        color="gray.100"
+        _hover={{ bg: "blue.700" }}
+        onClick={onOpen}
+      >
+        Show PDA
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent bg="#151515" maxW="70%" maxH="90vh" overflowY="auto">
+          <ModalHeader color="gray.100">Pushdown Automata</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
             {!prob2 ? (
               <VStack>
-
                 <Box>
-                  <img src="/views/PDA1.jpg"></img>
+                  <img src="/views/PDA1.jpg" alt="PDA1" />
                 </Box>
               </VStack>
             ) : (
               <VStack>
                 <Box>
-                  <img src="/views/PDA2.jpg"></img>
+                  <img src="/views/PDA2.jpg" alt="PDA2" />
                 </Box>
                 {/* Add more images as needed */}
               </VStack>
             )}
-          </PopoverBody>
-        </PopoverContent>
-      </Popover>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
